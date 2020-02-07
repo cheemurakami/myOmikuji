18948148828ba12b9238c3cee1f85cb03508fd13
Chisato Murakami <kiwipom1031@gmail.com>
2020-02-07 15:44:00 -0800

    added messages

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
 'use strict';
 {
- const myLuck = ['大吉','中吉','末吉','小吉','凶','大凶'];
+ const myLuckNames = ['大吉','中吉','末吉','小吉','凶','大凶'];
  
- 
+ const myLuckObj = {
+     '大吉': 'わーいやったー',
+     '中吉': 'やったー',
+     '末吉': '微妙',
+     '小吉': 'そんな',
+     '凶': 'どーん',
+     '大凶': 'どどーん'
+ };
+
  const btn = document.getElementById('btn');
+ const message = document.getElementById('message');
+
  btn.addEventListener('click', () => {
-     let myLuckNum = Math.floor(Math.random() * myLuck.length);  
-     btn.innerHTML = (myLuck[myLuckNum]);
+     let myLuckNum = Math.floor(Math.random() * myLuckNames.length);  
+     btn.innerHTML = (myLuckNames[myLuckNum]);
+
+     let resultMessage = myLuckObj[myLuckNames[myLuckNum]];
+     message.innerHTML = (resultMessage);
 });
 
 }
