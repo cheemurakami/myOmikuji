adec513bb918c743674bf2d8fcb1ecf3f74c0fba
Chisato Murakami <kiwipom1031@gmail.com>
2020-02-07 12:59:14 -0800

    can click more

--- js/main.js
+++ js/main.js
@@ -1,15 +1,12 @@
 'use strict';
-
 {
  const myLuck = ['大吉','中吉','末吉','小吉','凶','大凶'];
- let myLuckNum = Math.floor(Math.random() * myLuck.length);  
  
-
+ 
  const btn = document.getElementById('btn');
  btn.addEventListener('click', () => {
-    btn.innerHTML = (myLuck[myLuckNum]);
+     let myLuckNum = Math.floor(Math.random() * myLuck.length);  
+     btn.innerHTML = (myLuck[myLuckNum]);
 });
-
-
 
 }
